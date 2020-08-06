27eb8003900b2599f75459a770ce68d1510ba09d
AbdoSobhy <AbdoAhmedSobhy@hotmail.com>
2020-08-06 21:30:00 +0200

    after stash

--- StashSession/ViewController.swift
+++ StashSession/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let area1 = Area()
+        print("area: \(area1.area)")
     }
 
 
