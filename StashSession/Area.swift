e5968b76b3199ef3e2bb20696e61afe57c0bba9c
AbdoSobhy <AbdoAhmedSobhy@hotmail.com>
2020-08-06 21:18:13 +0200

    area

---
+++ StashSession/Area.swift
@@ -0,0 +1,13 @@
+//
+//  Area.swift
+//  StashSession
+//
+//  Created by Abdelrahman Sobhy on 8/6/20.
+//  Copyright © 2020 Macbook pro. All rights reserved.
+//
+
+import Foundation
+
+class Area {
+    let area = 10.0
+}
